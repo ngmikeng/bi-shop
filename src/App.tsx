62d79a59f3e91298ac331165c57c4dc236304202
Minh Nguyen <ngmikeng@gmail.com>
2021-09-05 16:09:49 +0700

    Add header, logo svg, menu link

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.scss';
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="logo-heading">Bi Shop</h1>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
